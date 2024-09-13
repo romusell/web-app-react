1b5c0b230b84f4a6e13f5ca94ee2bc21c4a499c5
Roman Kovalchuk <romusell@gmail.com>
2024-09-13 11:49:21 +0300

    initial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = ()=>{
       
-    const{user, onClose} = useTelegram;
+    const{user, onClose} = useTelegram();
    
     return(
     <div className={"header"}>
