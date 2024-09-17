a56920a8d8f3ad6c91dbffc84f9833536e3e3ef0
Roman Kovalchuk <romusell@gmail.com>
2024-09-17 16:28:53 +0300

    initial

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "../Button/Button";
+import './ProductItem.css';
+
+const ProductItem = ({ture, className,onAdd}) => {
+
+    const onAddHandler = ()=>{
+        onAdd(ture);
+    };
+
+    return(
+    <div className={'ture' + className}>
+         <div className={"img"}>{ture.picture}</div>
+         <div className={"title"}>{ture.title}</div>
+         
+         <div className={"description"}>{ture.description}</div>
+         <div className={'price'}>
+            <span>Ціна Єкскурсії <b>{ture.price}</b></span>
+         </div>
+         <Button className={'add-btn'} onClick={onAddHandler}>
+           Додати в Кошик
+         </Button>          
+     </div>
+    );
+};
+
+export default ProductItem;
