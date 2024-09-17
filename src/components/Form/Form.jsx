75a189a5d3cc13be28bfff3757901085530df36a
Roman Kovalchuk <romusell@gmail.com>
2024-09-17 10:33:09 +0300

    initial

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,21 +14,21 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
-    const onSendData = useCallback( ()=>{
-             const data = {
-                country,
-                street,
-                subject
-             } 
-            tg.sendData(JSON.stringify(data)) 
-    },[country,street,subject]);
-    
-    useEffect( ()=>{
-      tg.onEvent('mainButtonClicked', onSendData);
-      return () =>{
-        tg.offEvent('mainButtonClicked', onSendData);
-      }          
-    },[onSendData])
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
