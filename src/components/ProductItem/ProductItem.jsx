1cf40da001c5de7a9afa01d4672874b6c3f65fc0
Roman Kovalchuk <romusell@gmail.com>
2024-09-17 17:14:54 +0300

    initial

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -12,9 +12,9 @@ const ProductItem = ({ture, className,onAdd}) => {
 
     return(
     <div className={'ture' + className}>
-         <div className={"img"}></div>
-         <div className={"title"}>{ture.title}</div>
-         <div className={"description"}>{ture.description}</div>
+         <div className={"img"}>{ture.picture}</div>
+         <div className={"title"}>{ture.name}</div>
+         <div className={"description"}>{ture.discription}</div>
          <div className={'price'}>
             <span>Ціна Єкскурсії <b>{ture.price}</b></span>
          </div>
